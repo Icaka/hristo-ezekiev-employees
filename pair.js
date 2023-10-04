e34b7d01cf824cc286d39256ea8082d40da5732c
Icaka <ico.ezekiev@gmail.com>
2023-10-04 16:20:20 +0300

    Creating the pairs with overlapping projects

--- pair.js
+++ pair.js
@@ -25,3 +25,22 @@ class Pair {
         return sum;
     }
 }
+
+function getOverlapDays(startDate1, endDate1, startDate2, endDate2) {
+    startDate1 = new Date(startDate1);
+    endDate1 = new Date(endDate1);
+    startDate2 = new Date(startDate2);
+    endDate2 = new Date(endDate2);
+  
+    const maxStartDate = new Date(Math.max(startDate1, startDate2));
+    const minEndDate = new Date(Math.min(endDate1, endDate2));
+  
+    const overlapInMilliseconds = minEndDate - maxStartDate;
+    const overlapInDays = overlapInMilliseconds / (1000 * 60 * 60 * 24); // overlap in days
+  
+    if (overlapInDays > 0) {
+        return overlapInDays;
+    } else {
+        return 0; // No overlap
+    }
+  }
--- script.js
+++ script.js
@@ -17,10 +17,71 @@ document.getElementById('processButton').addEventListener('click', () => {
         console.log('csvData: ');
         console.log(csvData);
         console.log('Processed data: ');
-        console.log(processData(csvData));
+
+		const data = processData(csvData); 
+
+        console.log(data);
+
+		console.log('pairs ');
+		const allPairs = createPairs(data);
+		
 
         resultContainer.textContent = 'CSV data loaded and processed.';
     };
 
     reader.readAsText(file);
 });
+
+function createPairs(data)
+{
+	pairs = [];
+	for(let i = 0; i < data.length; i++) {
+		let person1 = data[i];
+		//console.log(person1);
+		//console.log(person1.empID);
+		for(let j = i + 1; j < data.length; j++) {
+			let person2 = data[j];
+
+			if(person1.projectID == person2.projectID) { // checking for common project
+				const startDate1 = person1.dateFrom;
+				const startDate2 = person2.dateFrom;
+				const endDate1 = person1.dateTo;
+				const endDate2 = person2.dateTo;
+				//console.log(`${startDate1}, ${startDate2}, ${endDate1}, ${endDate2}`)
+				const duration = getOverlapDays(startDate1, endDate1, startDate2, endDate2);
+				//console.log(`DURATION: ${duration}`)
+				if(duration > 0) {			
+					//console.log('PAIR FOUND');	
+					addToPairs(pairs, person1.empID, person2.empID, person1.projectID, duration);
+				}
+			}
+		}
+	}
+	console.log(pairs);
+	return pairs;
+}
+
+function addToPairs(pairs, emp1, emp2, projId, duration) {
+	if(emp1 == emp2)
+		return;
+	for(const pair of pairs) {
+		if(pair.isEqual(emp1, emp2)) {
+			pair.addProject(projId, duration);
+			return;
+		}
+	}
+	const pair = new Pair(emp1, emp2, projId, duration);
+	pairs.push(pair);
+}
+
+function getLongestCollab(pairs) {
+	let longestDuration = 0;
+	let result;
+	for(let pair in pairs) {
+		if(pair.getFullDuration() > longestDuration) {
+			longestDuration = pair.getFullDuration();
+			result = pair;
+		}
+	}
+	return result;
+}
